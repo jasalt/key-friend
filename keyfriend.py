47fa6d14b8878f961dc35ee5a4d8327f8256face
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-09-15 23:07:26 +0300

    Fix development setup. Python 3 unsupported.

--- keyfriend.py
+++ keyfriend.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Fetch song metadata from Echonest. Fallback to keyfinder.
 # Misc features for automating work with digital recordings
 
@@ -20,7 +21,38 @@
 # - Downloader
 #    youtube-dl spotify-onthego spotify-ripper etc.
 
+
+# from pyechonest import song
+import click
+
+from pyechonest import config
+from secrets import ECHONEST_KEY
 from pyechonest import song
-s = song.search(artist='bluescreens', title='strike one')
-it = s[1]
-it.get_audio_summary()
+from toolz import take
+
+config.ECHO_NEST_API_KEY = ECHONEST_KEY
+
+
+def print_song_entry(s):
+    # import ipdb; ipdb.set_trace()
+    print(s)
+
+
+@click.command()
+# @click.option('--artist', default=None, help='Artist name')
+@click.option('--query', prompt='Type search query for artist and title',
+              help='Search by artist and title')
+def song_info(query):
+    result = song.search(combined=query)
+    print("Got %s results." % len(result))
+    map(lambda entry: print_song_entry(entry), take(5, result))
+    # it = s[1]
+    # it.get_audio_summary()
+    # mode (int): 0 or 1 (minor or major)
+    # key (int): 0-11 (c, c-sharp, d, e-flat, e, f, f-sharp, g, a-flat, a,
+    # b-flat, b)
+
+
+if __name__ == '__main__':
+    song_info()
+    
