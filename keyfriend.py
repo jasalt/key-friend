34b2db494e0ec1157bdd06d9dde3439deecc3207
Jarkko Saltiola <jarkko.saltiola@koodilehto.fi>
2015-09-15 21:42:34 +0300

    Some notes.

--- keyfriend.py
+++ keyfriend.py
@@ -4,12 +4,21 @@
 # TODO
 # - Parse input files
 # - Parse file metadata
+# - Command Line Interface
+#    http://click.pocoo.org/5/
 # - Fetch data from echonest
+#    https://github.com/echonest/pyechonest
+#    http://developer.echonest.com/raw_tutorials/faqs/faq_04.html
 # - Local keyfinder software libary integration
+#    http://www.ibrahimshaath.co.uk/keyfinder/
+#    C++ lib https://github.com/ibsh/libKeyFinder
 # - Drag Drop UI with Qt4 or something
+#    http://zetcode.com/gui/pyqt4/dragdrop/
 # - Playlist view
 # - Harmonic playlist sorting
+#    http://djtechtools.com/2013/12/20/advanced-key-mixing-techniques-for-djs/
 # - Downloader
+#    youtube-dl spotify-onthego spotify-ripper etc.
 
 from pyechonest import song
 s = song.search(artist='bluescreens', title='strike one')
